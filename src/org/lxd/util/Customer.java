e5f5501f0be83e7b270b311ac7768878eaed74ee
lxd <a5834099147@126.com>
2014-10-12 16:03:09 +0800

    移除无用的局部变量, 局部变量很容易引发问题

--- src/org/lxd/util/Customer.java
+++ src/org/lxd/util/Customer.java
@@ -55,11 +55,7 @@ public class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = rentals.nextElement();
-			
-			///< datermine amounts for each line
-			thisAmount = each.getCharge();
+			Rental each = rentals.nextElement();				
 			
 			///< add frequent renter points
 			frequentRenterPoints++;
@@ -69,8 +65,8 @@ public class Customer {
 			}
 			
 			///< show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		
 		///< add footer lines
